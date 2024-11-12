5b6641295aede08eacf058e9fa28c75bb0cd28c2
Daniel Santamarta <danisantamarta5@gmail.com>
2024-11-12 14:08:25 +0100

    cambios 12/11/2024

--- create_character.php
+++ create_character.php
@@ -1,3 +1,31 @@
+<?php
+require_once 'configuracion/db.php';
+require_once("./model/Character.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $character = new character();
+    $character  ->setName($_POST['name']) 
+                ->setDescription($_POST['description']);
+   
+    echo "Nombre introducido: ". $_POST["name"]."<br>";
+    echo "Descripcion introducida: ". $_POST["description"]."<br>";
+
+    $name = $_POST["name"];
+    $description = $_POST["description"];
+
+    $stmt = $db->prepare("INSERT INTO characters (name, description) 
+        VALUES (:name, :description)");
+    $stmt->bindParam(':name',$name);
+    $stmt->bindParam(':description',$description);
+
+    if($stmt->execute()){
+        echo"Personaje creado";
+    }else{
+        echo "ERROR";
+    }
+
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +35,7 @@
 </head>
 <body>
     <h1>Crea tu personaje</h1>
-    <form action="save-character.php" method="POST">
+    <form action="create-character.php" method="POST">
         <input name="name" type= "text" placeholder="nombre">
         <input name="description" type= "text" placeholder="descripcion">
         <input type= "submit">
